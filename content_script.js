772604f25a2385ab3de1c0cfb1cf936f26d4fa86
Roberto Carta <robertocarta.mail@gmail.com>
2023-11-18 16:31:11 +0100

    words function

--- content_script.js
+++ content_script.js
@@ -2,20 +2,39 @@ var pattern;
 var replacement;
 
 const words = {
-  "Fisco": "lol",
-  "loro": "their",
-    "tempo": "time",
-    "se": "if",
-    "volontà": "will",
-    "come": "how",
-    "detto": "said",
-    "un": "an",
-    "ogni": "each",
-    "dire": "tell",
-    "fa": "does",
-    "fisso": "set",
-    "tre": "three",
-    "desiderare": "desire",
+  "Fisco": ["lol"],
+  "loro": ["their"],
+    "tempo": ["time"],
+    "se": ["if"],
+    "volontà": ["will"],
+    "come": ["how"],
+    "detto": ["said"],
+    "un": ["an"],
+    "ogni": ["each"],
+    "dire": ["tell"],
+    "fa": ["does"],
+    "fisso": ["set"],
+    "tre": ["three"],
+    "desiderare": ["desire"],
+}
+
+var knownWords = []
+
+// n words
+// wordSource
+function wordPairs(nWords, wordSource, knownWords) {
+  // return words.values.filter( vals => { knownWords.filter( w => vals.includes(w) ) } )
+  var cleanWords = {}
+  for ( var key in words  ) {
+    var translations = words[key]
+    for (var word in translations) {
+      if (knownWords.includes(word)) {
+        continue
+      }
+      cleanWords[key] = translations
+    }
+  }
+  return cleanWords
 }
 
 
@@ -26,9 +45,9 @@ var textNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, n
 while (textNodes.nextNode()) {
     console.log("hey")
     var node = textNodes.currentNode;
-    for (var word in words) {
+    for (var word in wordPairs()) {
       pattern =  new RegExp(`\\b${word}\\b`)
-      var replacement =  "<<<<<<<<<<<<<<< " + words[word] +  ">>>>>>>>>>>>>>"
+      var replacement =  "<<<<<<<<<<<<<<< " + words[word][0] +  ">>>>>>>>>>>>>>"
       node.textContent = node.textContent.replace(pattern, replacement)
     }
 }
