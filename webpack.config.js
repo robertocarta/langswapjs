955b26a390c9b901c038d74941a67d8dc659b64e
Federico Cirina <effedeso@gmail.com>
2023-11-21 16:28:53 +0000

    added new plugin to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,57 @@
 
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
 module.exports = {
   entry: './src/content_script.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@': `${__dirname}/src`,
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
   module: {
     rules: [
+      {
+      test: /\.vue$/,
+      use: {
+        loader: 'vue-loader'
+      }
+    },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
-      }
-    ]
-  }
+      },
+      // to read the css files
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      // to read the sass style block in vue files.
+      {
+        test: /\.sass$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      // to read the scss style block in vue files. 
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin()
+  ]
 };
